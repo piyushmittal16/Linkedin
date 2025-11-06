89e9075b5a9062d3b9e361f06bd105c384dea5db
Piyush Mittal <mittalp2380@gmail.com>
2025-11-07 03:12:32 +0530

    Successfull

--- Backend/index.js
+++ Backend/index.js
@@ -61,7 +61,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: [process.env.FRONTEND_URL],
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     credentials: true, // 🔥 Must have for cookies
   })
