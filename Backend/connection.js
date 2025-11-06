5293bd187d8f8076720cc5e26db74ca751df5ef3
Piyush Mittal <mittalp2380@gmail.com>
2025-11-06 15:50:01 +0530

    upgrade

--- Backend/connection.js
+++ Backend/connection.js
@@ -2,14 +2,12 @@ const mongoose = require("mongoose");
 
 const connectDB = async () => {
   try {
-    await mongoose.connect(
-      process.env.MONGODB_URI,
-      {
-        useNewUrlParser: true,
-        useUnifiedTopology: true,
-        serverSelectionTimeoutMS: 5000, // timeout if no connection in 5s
-      }
-    );
+    await mongoose.connect(process.env.MONGODB_URI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 5000, // timeout if no connection in 5s
+    });
+    console.log("DB Connected");
   } catch (error) {
     console.error("❌ MongoDB Connection Error:", error.message);
     process.exit(1); // stop app if not connected
